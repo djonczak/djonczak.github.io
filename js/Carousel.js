9e946b4ff20d29fc9257a32f3a18fe0b65e96f0a
Ted <djonczak1@gmail.com>
2020-09-13 00:51:54 +0100

    Changed projects cards in main pages

--- js/Carousel.js
+++ js/Carousel.js
@@ -5,12 +5,14 @@ carousel();
 function carousel() 
 {
 	var i;
-	var x = document.getElementsByClassName("parallax");
+	var x = document.getElementsByClassName("background-slide");
 	for (i = 0; i < x.length; i++) {
-		x[i].style.display = "none";
+    x[i].style.display = "none";  
 	}
 	myIndex++;
-	if (myIndex > x.length) {myIndex = 1}
-	x[myIndex-1].style.display = "block";
-	setTimeout(carousel, 2000); // Change image every 2 seconds
+	if (myIndex > x.length) {myIndex = 1}    
+	x[myIndex-1].style.display = "block";  
+	setTimeout(carousel, 9000);  
 }
+
+
